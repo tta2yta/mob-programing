e8f02eca3e4415b89ca078aff9a9c1973905a338
Tedros Tesfay Andemichael <tta2yta@gmail.com>
2021-09-04 00:56:32 +0300

    Create breakingRecords.js

---
+++ breakingRecords.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+function breakingRecords(scores) {
+    // Write your code here
+     let games = scores ;
+    let min = games[0];
+    let max = games[0];
+    
+    let minRecord = 0;
+    let maxRecord = 0;
+    
+    for (const score of games) {
+        if (max < score) {
+            max = score;
+            maxRecord++;
+        }
+        else if (score < min) {
+            min = score;
+            minRecord++;
+        }
+    }
+    
+    return [maxRecord, minRecord];
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const scores = readLine().replace(/\s+$/g, '').split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
